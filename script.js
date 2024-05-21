7dd08f6a57af8f4ff22bd6b2dc4dc3f939408ca7
Tiziano Di Felice <tizianofelice@gmail.com>
2024-05-21 12:54:57 +0200

    mancate solo immagine alla home e footer

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 const navbar = document.querySelector('#navbar');
-let scrollato = 0
+let clients = document.querySelector('#clients');
+let sedi = document.querySelector('#sedi');
+let collaboratori = document.querySelector('#collaboratori');
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY >= 300){
+    if(window.scrollY >= 20){
         // tutte le modifiche che vogliamo applicare quando scrolliamo verso il basso
         navbar.classList.remove('bg-body-dark');
+        navbar.classList.add('bg-body-light');
         navbar.setAttribute('data-bs-theme', 'light');
     } else {
         // tutte le modifiche che vogliamo applciare quando torniamo su
         navbar.classList.add('bg-body-dark');
+        navbar.classList.remove('bg-body-light');
         navbar.setAttribute('data-bs-theme', 'dark');
     }
 })
 
-navbar.addEventListener('click', () => {
-    if(scrollato >= 300){
-        window.scrollTo({ top: scrollato - 50, behavior: 'smooth' });
-        scrollato -= 50;
-    } else {
-        window.scrollTo({ top: scrollato + 50, behavior: 'smooth' });
-        scrollato += 50;
-    }
-    
-})
+
+// chiamta asincrona = chiamta che non viene eseguita in contemporanea al resto del codice
+// built-in-function 
+// setInterval() = crea un loop infinito ripetendo un blocco di codice all'infinito ogni tot intervallo di tempo
+// clearInterval() = altra chiamata asincrona che inerrompe un altro loop infinito lanciato da setInterval
+
+function numeroIncrementato(num, element, intervallo){
+    let counter = 0;
+    let interval = setInterval(() => {
+        if(counter < num){
+            counter++;
+            element.innerHTML = counter;
+        } else {
+            clearInterval(interval);
+        }
+    }, intervallo)
+}
+
+numeroIncrementato(500, clients, 10);
+numeroIncrementato(50, sedi, 50);
+numeroIncrementato(400, collaboratori, 10);
